a6ae31417c2b604d0c89365afb0a5271fafd2a97
hujiawei <hujiawei@aobi.com>
2019-10-14 19:46:36 +0800

    [10.14]percetron 修改

--- 20191014_Perceptron.py
+++ 20191014_Perceptron.py
@@ -3,6 +3,34 @@ import numpy as np
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 
+class Model:
+    def __init__(self):
+        self.w = np.ones(len(data[0]) - 1, dtype=np.float32)
+        self.b = 0
+        self.l_rate = 0.1
+
+    def sign(self, x, w, b):
+        y = np.dot(x, w) + b
+        return y
+
+    def fit(self, X_train, y_train):
+        is_wrong = False
+        while not is_wrong:
+            wrong_count = 0
+            for d in range(len(X_train)):
+                X = X_train[d]
+                y = y_train[d]
+                if y * self.sign(X, self.w, self.b) <= 0:
+                    self.w = self.w + self.l_rate * np.dot(y, X)
+                    self.b = self.b + self.l_rate * y
+                    wrong_count += 1
+            if wrong_count == 0:
+                is_wrong = True
+        return 'Perceptron Model!'
+
+    def score(self):
+        pass
+
 iris = load_iris()
 # print(iris)
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
@@ -16,43 +44,14 @@ df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'lab
 # plt.legend()
 # plt.show()
 data = np.array(df.iloc[:100, [0,1,-1]])
-x, y = data[:,:-1], data[:,-1]
+x,  y= data[:,:-1], data[:,-1]
 y = np.array([1 if i == 1 else -1 for i in y])
-class Model:
-    def __init__(self):
-        self.w = np.ones(len(data[0])-1, dtype=np.float32)
-        self.b = 0
-        self.l_rate = 0.1
-
-    def sign(self, x, w, b):
-        y = np.dot(x, w) + b
-        return y
-
-    def fit(self, x_train, y_train):
-        is_wrong = False
-        while not is_wrong:
-            wrong_count = 0
-            for d in range(len(x_train)):
-                x = x_train[d]
-                y = y_train[d]
-                if y * self.sign(x, self.w, self.b) <= 0:
-                    self.w = self.w + self.l_rate * np.dot(y, x)
-                    self.b = self.b + self.l_rate * y
-                    wrong_count += 1
-            if wrong_count == 0:
-                print('Perceptron Model!')
-            return 'Perceptron Model!'
-
-    def score(self):
-        pass
-
 perceptron = Model()
 print(x, y)
 perceptron.fit(x, y)
-x_points = np.linspace(4, 7,10)
-print(perceptron.w, perceptron.b)
+x_points = np.linspace(4, 7,100)
 y_ = -(perceptron.w[0]*x_points + perceptron.b)/perceptron.w[1]
-plt.plot(x_points, y_)
+plt.plot(x_points, y_, label='line')
 
 plt.plot(data[:50, 0], data[:50, 1], 'bo', color='blue', label='0')
 plt.plot(data[50:100, 0], data[50:100, 1], 'bo', color='orange', label='1')
